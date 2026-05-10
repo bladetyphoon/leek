8b610293abf36d1f9362b1317fa169ea22a698c3
bladetyphoon <120764054+bladetyphoon@users.noreply.github.com>
2026-05-10 12:28:46 -0700

    Update love.js

--- games/data/love.js
+++ games/data/love.js
@@ -699,7 +699,7 @@ var Love = (function () {
 		function isFileURI(filename) {
 			return hasPrefix(filename, fileURIPrefix);
 		}
-		var wasmBinaryFile = "https://cdn.jsdelivr.net/gh/bladetyphoon/leek@main/games/data/love.wasm";
+		var wasmBinaryFile = "love.wasm";
 		if (!isDataURI(wasmBinaryFile)) {
 			wasmBinaryFile = locateFile(wasmBinaryFile);
 		}
